2b9c3c68c73708c7398586962646e0ce4f5e0568
StephenSales <sdsalesstpn@gmail.com>
2024-11-18 15:54:25 +0800

    logout fixed

--- karerun/Homepage/views.py
+++ karerun/Homepage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from RegLogCreate.models import Event,User
 
@@ -14,3 +14,7 @@ def homepage(request):
     else:
         print("no session")
     return render(request, 'homepage.html', {'user': user, 'events':events})
+
+def logout(request):
+    del request.session['userID']
+    return redirect('index')
