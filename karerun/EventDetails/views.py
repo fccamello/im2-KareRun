6ccb675009841d21fd8cb85102e022a775f96868
StephenSales <sdsalesstpn@gmail.com>
2024-11-25 13:27:25 +0800

    updated organizer view

--- karerun/EventDetails/views.py
+++ karerun/EventDetails/views.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 
 def event_detail(request, event_id):
     event = get_object_or_404(Event, eventid=event_id) 
+    userId = request.session.get('userID',None)
     eventname_split = event.eventname.split(' ', 1)
     first_word = eventname_split[0]
     rest_of_text = " ".join(eventname_split[1:]) if len(eventname_split) > 1 else ''
@@ -46,6 +47,7 @@ def event_detail(request, event_id):
     print("Banner Image URL:", event.bannerimage.url) 
     return render(request, 'event_details.html', {
         'event': event,
+        'userID': userId,
         'first_word': first_word,
         'rest_of_text': rest_of_text,
         'categories': category_list,
